45a4bfcbcb998b60ba9c372b71208530f059138c
MaxSuslov <maxsusmail@gmail.com>
2022-10-05 22:19:38 +0200

    Using Vue instead of Vanilla JS - .mount('#app') after Vue.createApp({data etc.})

--- gs-01-starting-project/app.js
+++ gs-01-starting-project/app.js
@@ -1,17 +1,33 @@
-const buttonEl = document.querySelector('button');
-const inputEl = document.querySelector('input');
-const listEl = document.querySelector('ul');
+Vue.createApp({
+  data() {
+    return {
+      goals: [],
+      enteredValue: ''
+    };
+  },
+  methods: {
+    addGoal() {
+      this.goals.push(this.enteredValue);
+      this.enteredValue = '';
+    }
+  }
+  // we need to define which part of the page should be controlled by Vue app. In our case div#app contains all Vue-related code. In mount we use a string containing the CSS-like selector
+}).mount('#app');
 
-function addGoal() {
-  // .value is a built-in property on JS input elements. We can extract if from the user input.
-  const enteredValue = inputEl.value;
-  // .createElement is a standard JS method
-  const listItemEl = document.createElement('li');
-  listItemEl.textContent = enteredValue;
-  // just append() would also work
-  listEl.appendChild(listItemEl);
-  // clear the input!
-  inputEl.value = '';
-}
+// const buttonEl = document.querySelector('button');
+// const inputEl = document.querySelector('input');
+// const listEl = document.querySelector('ul');
 
-buttonEl.addEventListener('click', addGoal);
+// function addGoal() {
+//   // .value is a built-in property on JS input elements. We can extract if from the user input.
+//   const enteredValue = inputEl.value;
+//   // .createElement is a standard JS method
+//   const listItemEl = document.createElement('li');
+//   listItemEl.textContent = enteredValue;
+//   // just append() would also work
+//   listEl.appendChild(listItemEl);
+//   // clear the input!
+//   inputEl.value = '';
+// }
+
+// buttonEl.addEventListener('click', addGoal);
