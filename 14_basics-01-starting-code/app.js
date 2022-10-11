6a4d91ef1e4132cb7c92665b53a2869273832a84
MaxSuslov <maxsusmail@gmail.com>
2022-10-11 22:55:34 +0200

    Output different text randomly

--- 14_basics-01-starting-code/app.js
+++ 14_basics-01-starting-code/app.js
@@ -4,6 +4,17 @@ const app = Vue.createApp({
       courseGoal: 'Finish the course and learn Vue',
       vueLink: 'https://vuejs.org/'
     };
+  },
+  methods: {
+    outputGoal() {
+      // calculate a random number between 0 and 1
+      const randomNumber = Math.random();
+      if (randomNumber < 0.5) {
+        return 'Learn Vue!';
+      } else {
+        return 'Master Vue!';
+      }
+    }
   }
 });
 
