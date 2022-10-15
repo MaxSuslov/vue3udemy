a95e182737ad01d61a41b56ca895a80bf6d4bd49
MaxSuslov <maxsusmail@gmail.com>
2022-10-15 21:25:09 +0200

    Transferring the logic into the app.js from html

--- basics-03-events-starting-code/app.js
+++ basics-03-events-starting-code/app.js
@@ -1,9 +1,17 @@
 const app = Vue.createApp({
   data() {
     return {
-      counter: 0,
+      counter: 0
     };
   },
+  methods: {
+    add() {
+      return this.counter++;
+    },
+    reduce() {
+      return (this.counter = this.counter - 1);
+    }
+  }
 });
 
 app.mount('#events');
