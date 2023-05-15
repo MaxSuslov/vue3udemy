cc5f01baa666d96a39b63928b6c6bfbe9002e6ad
MaxSuslov <maxsusmail@gmail.com>
2023-05-15 22:09:58 +0200

    25 - event modifiers

---
+++ basics-05-using-the-native-event-object/app.js
@@ -0,0 +1,29 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      counter: 0,
+      name: '',
+      confirmedName: '',
+    };
+  },
+  methods: {
+    confirmInput() {
+      this.confirmedName = this.name;
+    },
+    submitForm(event) {
+      alert('Submitted!');
+    },
+    setName(event) {
+      this.name = event.target.value;
+    },
+    add(num) {
+      this.counter = this.counter + num;
+    },
+    reduce(num) {
+      this.counter = this.counter - num;
+      // this.counter--;
+    },
+  },
+});
+
+app.mount('#events');
