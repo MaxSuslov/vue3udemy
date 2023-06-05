c93b8e9cd95f55f6751d1151d851aac6f3da8112
MaxSuslov <maxsusmail@gmail.com>
2023-06-05 22:25:26 +0200

    attackMonster, attackPlayer methods reducing health by a random number

---
+++ prj-monster-01-starting-setup/app.js
@@ -0,0 +1,29 @@
+function getRandomValue(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+const app = Vue.createApp({
+  data() {
+    return {
+      playerHealth: 100,
+      monsterHealth: 100
+    };
+  },
+  methods: {
+    attackMonster() {
+      // calculate a random nr. between 5 and 12
+      // Math.floor(Math.random() * (max - min)) + min;
+      const attackValue = getRandomValue(5, 12);
+      this.monsterHealth -= attackValue;
+      this.attackPlayer();
+    },
+    // monster attacks player and hits harder
+    attackPlayer() {
+      const attackValue = getRandomValue(15, 8);
+      this.playerHealth -= attackValue;
+      this.attackMonster();
+    }
+  }
+});
+
+app.mount('#game');
