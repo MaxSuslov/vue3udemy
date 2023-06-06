d3168a28ee800c3ca7f82f062921f67e32752eb8
MaxSuslov <maxsusmail@gmail.com>
2023-06-06 16:05:16 +0200

    Special attack - available only every 3 rounds

--- prj-monster-01-starting-setup/app.js
+++ prj-monster-01-starting-setup/app.js
@@ -6,20 +6,25 @@ const app = Vue.createApp({
   data() {
     return {
       playerHealth: 100,
-      monsterHealth: 100
+      monsterHealth: 100,
+      currentRound: 0,
     };
   },
   // outsource the logic from html to computed property
   computed: {
     monsterBarStyles() {
-      return { width: this.monsterHealth + '%' };
+      return {width: this.monsterHealth + '%'};
     },
     playerBarStyles() {
-      return { width: this.playerHealth + '%' };
+      return {width: this.playerHealth + '%'};
+    },
+    mayUseSpecialAttack() {
+      return this.currentRound % 3 !== 0
     }
   },
   methods: {
     attackMonster() {
+      this.currentRound++;
       // calculate a random nr. between 5 and 12
       // Math.floor(Math.random() * (max - min)) + min;
       const attackValue = getRandomValue(5, 12);
@@ -30,6 +35,12 @@ const app = Vue.createApp({
     attackPlayer() {
       const attackValue = getRandomValue(15, 8);
       this.playerHealth -= attackValue;
+    },
+    specialAttackMonster() {
+      this.currentRound++;
+      const attackValue = getRandomValue(10, 25);
+      this.monsterHealth -= attackValue;
+      this.attackPlayer(); 
     }
   }
 });
